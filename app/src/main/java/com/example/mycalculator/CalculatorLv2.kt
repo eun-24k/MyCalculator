e0a4e1ecab8df9f2d30dffce7abeaa3e7d97a82b
Euny Kim <euny4535@gmail.com>
2023-11-29 20:42:31 +0900

    lv2 task still in Progress - indexing wrong since the numbers can have multiple digits - additionalCalculation method needs double checking

--- app/src/main/java/com/example/mycalculator/CalculatorLv2.kt
+++ app/src/main/java/com/example/mycalculator/CalculatorLv2.kt
@@ -1,27 +1,54 @@
 package com.example.mycalculator
 // Level 2
-// 더하기, 빼기, 나누기, 곱하기 연산 수행하기
+// Lv1에서 만든 Calculator 클래스에 출력 이후 추가 연산을 가능하도록 코드를 추가하고, 연산 진행 후 출력하기
 fun main(){
-    print("Type the equation without space: ")
-    val stringInput = readLine()!!
-    val inputArray = stringInput.toCharArray()
+    print("빈칸 없이 식을 작성하세요 ('+', '-', '*', '/') : ")
+    var stringInput = readLine()!!.toCharArray()
 
-    // first calculation
-    var operator = inputArray[1]
-    var number1 = inputArray[0].digitToInt().toDouble()
-    var number2 = inputArray[2].digitToInt().toDouble()
+    // 첫번째 계산
+    var operator = stringInput[1]
+    var number1 = stringInput[0].digitToInt().toDouble()
+    var number2 = stringInput[2].digitToInt().toDouble()
     var finalResult = calculation(number1, number2, operator)
 
-    // calculation iteration
-    if (inputArray.size > 3) {
-        for (i in 3 until inputArray.size step 2) {
-            operator = inputArray[i]
-            number1 = finalResult
-            number2 = inputArray[i + 1].digitToInt().toDouble()
+    // 계산 반복
+    finalResult = additionalCalculation(stringInput, finalResult, 3)
 
-            finalResult = calculation(number1, number2, operator)
+    println("계산 결과는 $finalResult 입니다.")
+
+    // 추가 계산
+    while (true) {
+        print("추가적인 계산을 하시겠습니까? (yes/no) : ")
+        var readIteration = readLine()!!
+        when (readIteration) {
+            in "no" -> break
+            in "yes" -> {
+                print("추가 연산을 작성하세요 : Ans")
+                stringInput = readLine()!!.toCharArray()
+                finalResult = additionalCalculation(stringInput, finalResult, 0)
+                println("계산 결과는 $finalResult 입니다.")
+            }
         }
     }
-    println("Result of the Calculation is $finalResult")
+}
+fun calculation(number1: Double, number2: Double, operator: Char): Double {
+    var result = 0.0
+    when (operator) {
+        in "+" -> result = number1 + number2
+        in "-" -> result = number1 - number2
+        in "/" -> result = number1 / number2
+        in "*" -> result = number1 * number2
+    }
+    return result
 }
 
+fun additionalCalculation(stringInput: CharArray, finalResult: Double, startIndexValue: Int): Double{
+    if (stringInput.size >= 3) {
+        for (i in startIndexValue until stringInput.size step 2) {
+            var operator = stringInput[i]
+            var number2 = stringInput[i + 1].digitToInt().toDouble()
+            var finalResult = calculation(finalResult, number2, operator)
+        }
+    }
+    return finalResult
+}
