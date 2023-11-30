a8baec5ddcf1dd3a68ccca739e0c555f28b35792
Euny Kim <euny4535@gmail.com>
2023-11-30 20:47:06 +0900

    lv3 task completed

--- app/src/main/java/com/example/mycalculator/CalculatorLv3.kt
+++ app/src/main/java/com/example/mycalculator/CalculatorLv3.kt
@@ -1,54 +1,43 @@
 package com.example.mycalculator
-// Level 3
-// AddOperation(더하기), SubstractOperation(빼기), MultiplyOperation(곱하기), DivideOperation(나누기) 연산 클래스를을 만든 후 클래스간의 관계를 고려하여 Calculator 클래스와 관계를 맺기
-//
-//관계를 맺은 후 필요하다면 Calculator 클래스의 내부코드를 변경하기
-//나머지 연산자(%) 기능은 제외합니다.
-//Lv2 와 비교하여 어떠한 점이 개선 되었는지 스스로 생각해 봅니다.
-//hint. 클래스의 책임(단일책임원칙)
-open class Calculator {
-    fun main(){
-        print("숫자를 입력하세요 : ")
-        var number1 = readLine()!!.toDouble()
-        print("연산자를 입력하세요 : ")
-        var operator = readLine()!!
-        print("숫자를 입력하세요 : ")
-        var number2 = readLine()!!.toDouble()
+
+//Lv3: AddOperation(더하기), SubstractOperation(빼기), MultiplyOperation(곱하기),
+// DivideOperation(나누기) 연산 클래스를을 만든 후 클래스간의 관계를 고려하여 Calculator 클래스와 관계를 맺기
+fun main() {
+    // 입력 값
+    print("숫자를 입력하세요 : ")
+    var number1 = readLine()!!.toDouble()
+    print("연산자를 입력하세요 : ")
+    var operator = readLine()!!
+    print("숫자를 입력하세요 : ")
+    var number2 = readLine()!!.toDouble()
 
 
-        // 첫번째 계산
-        var finalResult = calculation(number1, number2, operator)
+    // 객체 인스턴스화해주기
+    var addOperation = AddOperation(number1, number2)
+    var subtractOperation = SubtractOperation(number1, number2)
+    var multiplyOperation = MultiplyOperation(number1, number2)
+    var divideOperation = DivideOperation(number1, number2)
 
-        // 추가 계산
-        while (true) {
-            print("추가적인 계산을 하시겠습니까? (yes/no) : ")
-            var readIteration = readLine()!!
-            when (readIteration) {
-                in "no" -> break
-                in "yes" -> {
-                    print("연산자를 입력하세요 : ")
-                    operator = readLine()!!
-                    print("숫자를 입력하세요 : ")
-                    number2 = readLine()!!.toDouble()
-                    finalResult = calculation(finalResult, number2, operator)
-                    println("계산 결과는 $finalResult 입니다.")
-                }
-            }
-        }
-    }
-    fun calculation(number1: Double, number2: Double, operator: String): Double {
-        var result = 0.0
-        when (operator) {
-            in "+" -> result = AddOperation(number1, number2)
-            in "-" -> result = SubtractOperation(number1, number2)
-            in "/" -> result = DivideOperation(number1, number2)
-            in "*" -> result = MultiplyOperation(number1, number2)
-        }
-        return result
+    when (operator) {
+        in "+" -> println(addOperation.addition(number1, number2))
+        in "-" -> println(subtractOperation.subtraction(number1, number2))
+        in "/" -> println(divideOperation.division(number1, number2))
+        in "*" -> println(multiplyOperation.multiplication(number1, number2))
     }
 }
 
-class AddOperation(number1: Double, number2: Double): Double = number1 + number2
-class SubtractOperation(number1: Double, number2: Double): Double = number1 - number2
-class MultiplyOperation(number1: Double, number2: Double): Double = number1 * number2
-class DivideOperation(number1: Double, number2: Double): Double = number1 / number2
+class AddOperation(number1: Double, number2: Double) {
+    fun addition(number1: Double, number2: Double): Double = number1 + number2
+}
+
+class SubtractOperation(number1: Double, number2: Double) {
+    fun subtraction(number1: Double, number2: Double): Double = number1 - number2
+}
+
+class MultiplyOperation(number1: Double, number2: Double) {
+    fun multiplication(number1: Double, number2: Double): Double = number1 * number2
+}
+
+class DivideOperation(number1: Double, number2: Double) {
+    fun division(number1: Double, number2: Double): Double = number1 / number2
+}
